793e6df9e398bc05c9873315091ee05d8201c650
Cody Jett <27358696+C-Jett@users.noreply.github.com>
2024-02-14 23:12:22 -0600

    Removed extra options for eslint

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,12 @@
 const { Events } = require('discord.js');
 
 module.exports = {
-	// name: states which event this file is for
-	// once: property holds a boolean valuethat specifies if the event should run only once
-	// execute: holds event logic, which will be called by the event handler whenever the event emits
-	name: Events.ClientReady,
-	once: true,
-	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-	},
+  // name: states which event this file is for
+  // once: property holds a boolean valuethat specifies if the event should run only once
+  // execute: holds event logic, which will be called by the event handler whenever the event emits
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+    console.log(`Ready! Logged in as ${client.user.tag}`);
+  },
 };
