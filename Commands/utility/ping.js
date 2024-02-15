8cf1e911e8c0baa6b07abece57de4333b47f0961
Cody Jett <27358696+C-Jett@users.noreply.github.com>
2024-02-14 23:09:23 -0600

    Addition of a linter. So pretty!

--- Commands/utility/ping.js
+++ Commands/utility/ping.js
@@ -3,16 +3,16 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		// await interaction.reply({content: 'Pong!', emphemeral: true});
-		if (interaction.commandName === 'ping') {
-			await interaction.reply({ content: 'Secret Pong!', ephemeral: true });
-		}
-	},
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with Pong!'),
+    async execute(interaction) {
+        // await interaction.reply({content: 'Pong!', emphemeral: true});
+        if (interaction.commandName === 'ping') {
+            await interaction.reply({
+                content: 'Secret Pong!',
+                ephemeral: true,
+            });
+        }
+    },
 };
-
-
-
--- Commands/utility/roll.js
+++ Commands/utility/roll.js
@@ -3,47 +3,50 @@
   the die and display it for only that user
  */
 
-const { SlashCommandBuilder,EmbedBuilder, AttatchmentBuilder, bold } = require('discord.js');
-
+const {
+    SlashCommandBuilder,
+    EmbedBuilder,
+    AttatchmentBuilder,
+    bold,
+} = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('roll')
         .setDescription('Rolls a die')
-        .addStringOption(option =>
-             option.setName('sides')
-             .setDescription('The number of sides on the die')
-             .setRequired(true)
-             .addChoices(
-                {name: '4', value: '4'},
-                {name: '6', value: '6'},
-                {name: '8', value: '8'},
-                {name: '10', value: '10'},
-                {name: '12', value: '12'},
-                {name: '20', value: '20'},
-             )),
+        .addStringOption((option) =>
+            option
+                .setName('sides')
+                .setDescription('The number of sides on the die')
+                .setRequired(true)
+                .addChoices(
+                    { name: '4', value: '4' },
+                    { name: '6', value: '6' },
+                    { name: '8', value: '8' },
+                    { name: '10', value: '10' },
+                    { name: '12', value: '12' },
+                    { name: '20', value: '20' },
+                ),
+        ),
 
     async execute(interaction) {
         // gather the information, roll the dice
         const sides = interaction.options.getString('sides');
         const roll = Math.floor(Math.random() * sides) + 1;
-        const string = bold(`${interaction.member.displayName} , Rolled a ${sides}-sided die and got a ${roll} !`);
+        const string = bold(
+            `${interaction.member.displayName} , Rolled a ${sides}-sided die and got a ${roll} !`,
+        );
         console.log(string);
 
         // create the embed
         const exampleEmbed = new EmbedBuilder()
             .setTitle('Dice Roll')
-            .setColor(0x0099FF)
+            .setColor(0x0099ff)
             .setDescription(string)
             .setTimestamp()
-            .setImage('https://i.imgur.com/NyxGnsj.jpeg')
+            .setImage('https://i.imgur.com/NyxGnsj.jpeg');
 
         // send the embed
         await interaction.reply({ embeds: [exampleEmbed] });
     },
-
-
-    
 };
-
-
